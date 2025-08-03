cd95a776ae3a5918f265ef5b15748bfd22e29969
Rehan <dhanujarehan@gmail.com>
2025-08-03 19:31:08 +0530

    created array

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,5 +1,6 @@
+choices = ['r', 'p', 's']
 user_choice = input('Rock, Paper , Scissor? (r,p,s) :' ).lower()
 
-if user_choice != 'r' and user_choice != 'p' and user_choice != 's':
+if user_choice not in choices:
     print('Invalid choice. Please choose r, p, or s.')
     
