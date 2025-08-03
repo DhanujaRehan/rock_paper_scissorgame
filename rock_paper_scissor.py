5e30b4f4927c08a949971ade9b20ad6649a52173
Rehan <dhanujarehan@gmail.com>
2025-08-03 19:28:41 +0530

    first steps done

---
+++ rock_paper_scissor.py
@@ -0,0 +1,5 @@
+user_choice = input('Rock, Paper , Scissor? (r,p,s) :' ).lower()
+
+if user_choice != 'r' and user_choice != 'p' and user_choice != 's':
+    print('Invalid choice. Please choose r, p, or s.')
+    
